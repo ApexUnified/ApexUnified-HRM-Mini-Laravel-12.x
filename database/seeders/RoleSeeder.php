ec0822c7b0bb91a4df85ef620e7777c071db7ddb
Apex Unified <apexunified@gmail.com>
2025-03-11 17:53:49 +0500

    Role Seeder  Updated

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
 
 class RoleSeeder extends Seeder
@@ -14,16 +15,9 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         if (!Role::exists()) {
-            Role::create([
-                'name' => 'admin',
-                'guard_name' => 'web',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ], [
-                'name' => 'employee',
-                'guard_name' => 'web',
-                'created_at' => now(),
-                'updated_at' => now(),
+            Role::insert([
+                ["name" => "admin", "guard_name" => "web", "created_at" => now(), "updated_at" => now()],
+                ["name" => "employee", "guard_name" => "web", "created_at" => now(), "updated_at" => now()]
             ]);
         }
     }
